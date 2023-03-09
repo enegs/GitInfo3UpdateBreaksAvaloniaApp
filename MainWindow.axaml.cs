e5353df6c9aaddf1bf35b83b95a6dd865f07b892
Geoffray Schmetz <geoffray.schmetz@myocene.com>
2023-03-09 11:29:47 +0100

    Add project files.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace GitInfo3UpdateBreaksAvaloniaApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
